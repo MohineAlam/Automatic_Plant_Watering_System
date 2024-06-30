c8ecb3dcc54e1fb77e24856ece532ca57f8168ce
MohineAlam <mohine.tasrin.alam@gmail.com>
2024-06-30 22:03:59 +0100

    simplified test

--- test_sensor.py
+++ test_sensor.py
@@ -3,15 +3,12 @@ import explorerhat
 # read humidity from sensor - this should be attached to analogue (analog) one
 print("Running sensor test...")
 def read_sensor():
- read = input.input.one.read()
+ explorerhat.output.one.on()
+ time.sleep(1)
+ read = explorerhat.analog.one.read()
+ explorerhat.output.one.off()
  return read
 
-# Add voltage values to dry_voltage (completely dry soil) and wet_voltage (completey saturated soil) variables:
-dry_voltage = pass
-wet_voltage = pass
-def read_soil_moisture():
- current_voltage = read_sensor()
- percentage_moisture = ((current_voltage - dry_voltage) / (current_voltage - wet_voltage)) * 100
- return "The current humidity for your plant is: {percentage_moisture}%"
-
-print(read_soil_moisiture())
+# return read
+sensor_value = read_sensor()
+print(f"The voltage from you sensor is: {sensor_value}")
