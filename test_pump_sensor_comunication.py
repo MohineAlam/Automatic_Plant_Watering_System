5e1ee55047f2daea852c9ed9dce3ab0b63fd616f
MohineAlam <mohine.tasrin.alam@gmail.com>
2024-07-03 22:41:34 +0100

    initial commit

---
+++ test_pump_sensor_comunication.py
@@ -0,0 +1,35 @@
+# import your libraries
+import explorerhat
+import time
+
+# message to user
+print("Running water pump and sensor communication test...")
+
+# sensor function
+def sensor():
+	explorerhat.output.one.on()
+	time.sleep(1)
+	read = explorerhat.analog.one.read()
+	explorerhat.output.one.off()
+	return read
+
+# pump function with sensor function integrated
+def pump():
+	sensor_value = sensor()
+	saturated_soil = 0.4 # modify based on your plant
+	dry_soil = 0.04 # modify based on your plant
+	print(f"Your sensor value reads at: {sensor_value}V.")
+	if sensor_value >= saturated_soil:
+		print("Your plant is not thirsty! No watering is needed right now :)")
+	elif sensor_value <= dry_soil:
+		print("Your plant is thirsty! Watering your plant now :)")
+		explorerhat.motor.one.forwards()
+               	time.sleep(3)
+               	explorerhat.motor.one.off()
+		print("The thirst is quenched! Green love <3")
+	elif dry_soil < sensor_value < saturated_soil: 
+		print("Your plant will be due for a watering soon! No need to worry right now :)")
+
+# call pump() to execute function
+pump()
+print("The pump and sensor communication was a success! You can now switch on the automatic watering system!")
