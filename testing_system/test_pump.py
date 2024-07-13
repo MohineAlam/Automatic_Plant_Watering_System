91cbdf3b7d4292d098f3242adec80a7519de6842
MohineAlam <mohine.tasrin.alam@gmail.com>
2024-07-13 20:49:29 +0100

    included inputs and if statements for better execution

--- testing_system/test_pump.py
+++ testing_system/test_pump.py
@@ -1,15 +1,24 @@
 # import libraries
 import explorerhat
 import time
+import sys
 
-# message to user
-print("Running water pump test...")
+# ask if user is ready
+user_ready = input("Place pump into container full of water (make sure it is submerged) and place the pump tube into an empty container. Press (y) if you are ready or (n) to exit...").strip.lower()
+print(user_ready)
+
 # Switch on water pump from motor1
-speed = 50 # this can be edited for individual needs
-def water_pump_test():
- 	explorerhat.motor.one.forwards(speed)
- 	time.sleep(5) # switch on for 5 seconds
- 	explorerhat.motor.one.stop()
- 	print("Pump test was a success!")
+def water_pump_test(user_ready):
+	print("Running water pump test...")
+	speed = 50 # this can be edited for individual needs
+	if user_ready == "y":
+	 	explorerhat.motor.one.forwards(speed)
+	 	time.sleep(5) # switch on for 5 seconds
+ 		explorerhat.motor.one.stop()
+ 		print("Pump test was a success!")
+	else:
+		print("Pump test has been cancelled, run the pipeline again if you would like to test the pump.")
 
-water_pump_test()
+
+water_pump_test(user_ready)
+sys.exit()
--- testing_system/test_sensor.py
+++ testing_system/test_sensor.py
@@ -1,17 +1,29 @@
 # import your libraries
 import explorerhat
 import time
-# message to user
-print("Running sensor test...")
-# read humidity from sensor - this should be attached to analogue (analog) one
-def read_sensor():
-	explorerhat.output.one.on()
-	time.sleep(1)
-	read = explorerhat.analog.one.read()
-	explorerhat.output.one.off()
-	return read
+import sys
 
+# ask if user is ready
+user_ready = input("Place the electrodes (screws) into the soil of your plant, 5cm apart. When you are ready press (y) to continue or (n) to exit.").strip().lower()
+print(user_ready)
+# read humidity from sensor - this should be attached to analogue (analog) one
+def read_sensor(user_ready):
+	if user_ready == "y":
+		explorerhat.output.one.on()
+		time.sleep(1)
+		read = explorerhat.analog.one.read()
+		explorerhat.output.one.off()
+		return read
+	else:
+		print("The sensor test has been halted and exited. Run the script again to test the sensor.")
+		return None
 # return read
-sensor_value = read_sensor()
-print(f"The voltage from you sensor is: {sensor_value}V.")
-print("Your sensor is functioning as normal. Remember to test both dry and wet soil! Use the sensor_read_output.py script :)")
+sensor_value = read_sensor(user_ready)
+
+if sensor_value is not None:
+	print(f"The voltage from your sensor is: {sensor_value}V.")
+	print("Your sensor is functioning as normal. Remember to test both dry and wet soil! Use the sensor_read_output.py script :)")
+else:
+	print("Sensor value could not be read, ensure set up is correct and try again!")
+
+sys.exit()
