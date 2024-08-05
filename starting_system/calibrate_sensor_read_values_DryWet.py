f59c6cd7a25a0f53d0261037850d29499abc6a3e
MohineAlam <mohine.tasrin.alam@gmail.com>
2024-08-05 20:49:01 +0100

    clean up

--- starting_system/calibrate_sensor_read_values_DryWet.py
+++ starting_system/calibrate_sensor_read_values_DryWet.py
@@ -13,44 +13,50 @@ with open(log_file, mode = "w", newline = "") as file:
 
 # method for measuring dry soil
 def sensor_switch_dry():
-	user_prompt = input("Ready? Place the electrodes (screws) into the dry soil of your plant, once you're ready press (y) to continue or (n) to exit.")
-	if user_prompt == "y":
-		condition = "Dry Soil"
-		explorerhat.output.one.on()
-		time.sleep(2)
-		value = explorerhat.analog.one.read()
-		explorerhat.output.one.off()
-		print("The dry soil value has been recorded in the humidity_log file as: {}V.".format(value))
-		with open(log_file, mode = "a", newline = "") as file:
-	                writer = csv.writer(file)
-        	        writer.writerow([condition,value])
-	else:
-		print("Run script again to re-start")
-		return False
-
+	while True:
+		user_prompt = input("Ready? Place the electrodes (screws) into the dry soil of your plant, once you're ready press (y) to continue or (n) to exit.").strip().lower()
+		if user_prompt == "y":
+			condition = "Dry Soil"
+			explorerhat.output.one.on()
+			time.sleep(2)
+			value = explorerhat.analog.one.read()
+			explorerhat.output.one.off()
+			print("The dry soil value has been recorded in the humidity_log file as: {}V.".format(value))
+			with open(log_file, mode = "a", newline = "") as file:
+	                	writer = csv.writer(file)
+        	        	writer.writerow([condition,value])
+			break
+		elif:
+			print("Run script again to re-start")
+			return False
+		else:
+			print("Invalid input... press (y) to continue or (n) to exit.").strip().lower()
 
 # method for measuring wet soil
 def sensor_switch_wet():
-	user_prompt = input("Now lets measure the wet soil! Pour water into the soil until its saturated. Press (y) once you're ready or (n) to exit.")
-	if user_prompt == "y":
-                condition = "Wet Soil"
-                explorerhat.output.one.on()
-                time.sleep(2)
-                value = explorerhat.analog.one.read()
-                explorerhat.output.one.off()
-                print("The wet soil value has been recorded in the humidity_log file as: {}V.".format(value))
-		with open(log_file, mode = "a", newline = "") as file:
-                        writer = csv.writer(file)
-                        writer.writerow([condition,value])
-	else:
-               	print("Run script again to re-start")
-               	return False
-
+	while True:
+		user_prompt = input("Now lets measure the wet soil! Pour water into the soil until its saturated. Press (y) once you're ready or (n) to exit.").strip().lower()
+		if user_prompt == "y":
+                	condition = "Wet Soil"
+                	explorerhat.output.one.on()
+                	time.sleep(2)
+                	value = explorerhat.analog.one.read()
+                	explorerhat.output.one.off()
+                	print("The wet soil value has been recorded in the humidity_log file as: {}V.".format(value))
+			with open(log_file, mode = "a", newline = "") as file:
+                        	writer = csv.writer(file)
+                        	writer.writerow([condition,value])
+			break
+		elif:
+               		print("Run script again to re-start")
+               		return False
+		else:
+			print("Invalid input... Press (y) to continue or (n) to exit").strip().lower()
 
 # execution of functions:
 if sensor_switch_dry() is not False:
 	if sensor_switch_wet() is not False:
-		print("Measurements completed successfully!")
+		print("Measurements completed successfully! Make sure the .csv file has correctly recorded the soil measurements.")
 	else:
 		print("Wet soil measurement was not performed!")
 else:
--- starting_system/start_watering_system.py
+++ starting_system/start_watering_system.py
@@ -5,12 +5,20 @@ import csv
 
 # convert reading into humidity
 def convert_to_humidity(analogue_value):
-	min_analogue = 0.04 #dry soil read (write the path to the csv document you make from calibrate_sensor_read_values_DryWet.py)
-	max_analogue = 0.40 #saturated soil read (write the path to the csv document you make from calibrate_sensor_read_values_DryWet.py)
-	min_humidity = 0
-	max_humidity = 100
-	humidity = (analogue_value - min_analogue) * (max_humidity - min_humidity) / (max_analogue - min_analogue) + min_humidity
-	return humidity
+	while True:
+        	user_prompt = input("Place the pump in the water container (with the tube ontop of the soil of your plant). Place the electrodes (screws) 5cm apart in the soil of your plant. Once you are ready to continue press (y) or (n) to exit").strip().lower()
+		if user_prompt == "y":
+			min_analogue = "/watering_system/Automatic-Plant-Watering-System/starting_system/.csv" #dry soil read (write the path to the csv document you make from calibrate_sensor_read_values_DryWet.py)
+			max_analogue = "/watering_system/Automatic-Plant-Watering-System/starting_system/.csv"  #wet soil read (write the path to the csv document you make from calibrate_sensor_read_values_DryWet.py)
+			min_humidity = 0
+			max_humidity = 100
+			humidity = (analogue_value - min_analogue) * (max_humidity - min_humidity) / (max_analogue - min_analogue) + min_humidity
+			return humidity
+		elif user_prompt == "n":
+			print("Run the script again to restart!")
+			return False
+		else:
+			print("Invalid input... Press (y) to continue or (n) to exit.")
 
 # create log file and set up csv writer
 log_file = 'mositure_log.csv'
@@ -28,13 +36,12 @@ while True:
 	humidity = convert_to_humidity(analogue_value)
 	explorerhat.output.one.off()
 	if humidity < threshold:
-		print("Your plant is thirsy!!! Watering now...")
+		print("Your plant is thirsty!!! Watering now...")
 		explorerhat.output.two.on()
 		time.sleep(3)
 		explorerhat.output.two.off()
-		print("Finished watering! Your plant is no longer thirsy :D")
+		print("Finished watering! Your plant is no longer thirsty :D")
 		pump_status = "ON"
-	timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 	else:
 		print("Your plant is not thirsty! No need to water right now :)")
 		pump_status = "OFF"
