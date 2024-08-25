a08e2e02b80cf8e9d67072125a0399c82c3bff00
MohineAlam <mohine.tasrin.alam@gmail.com>
2024-08-25 21:26:53 +0100

    Function parameter name change and function call clean up

--- testing_system/test_explorerhat.py
+++ testing_system/test_explorerhat.py
@@ -1,14 +1,21 @@
 # import libraries
 import explorerhat
+import sys
 
-print("Explorer hat pro test run in progress...")
+user_input = input("Do you want to run the explorer hat test? If so press (y), if not press anything else to exit: ").strip().lower()
 
 # switch on all four explorer hat pro LEDs 
-def explorerhat_test(): 
- explorerhat.light.yellow.on()
- explorerhat.light.blue.on()
- explorerhat.light.red.on()
- explorerhat.light.green.on()
- print("Did you see the flash? If so, the explorer hat pro is working fine!")
+def explorerhat_test(question):
+	print("Explorer hat pro test run in progress...")
+	if question == "y":
+		explorerhat.light.yellow.on()
+		explorerhat.light.blue.on()
+		explorerhat.light.red.on()
+		explorerhat.light.green.on()
+		print("Did you see the flash? If so, the explorer hat pro is working fine!")
+	else:
+		print("You have exited the test. Run the script again to test the Explorer hat pro.")
 
-explorerhat_test()
+# call the function and exit system
+explorerhat_test(user_input)
+sys.exit()
--- testing_system/test_pump.py
+++ testing_system/test_pump.py
@@ -4,14 +4,13 @@ import time
 import sys
 
 # ask if user is ready
-user_ready = input("Place pump into container full of water (make sure it is submerged) and place the pump tube into an empty container. Press (y) if you are ready or (n) to exit...").strip.lower()
-print(user_ready)
+user_ready = input("Place pump into container full of water (make sure it is submerged) and place the pump tube into an empty container. Press (y) if you are ready or anythign else to exit: ").strip.lower()
 
 # Switch on water pump from motor1
-def water_pump_test(user_ready):
+def water_pump_test(question):
 	print("Running water pump test...")
 	speed = 50 # this can be edited for individual needs
-	if user_ready == "y":
+	if question == "y":
 	 	explorerhat.motor.one.forwards(speed)
 	 	time.sleep(5) # switch on for 5 seconds
  		explorerhat.motor.one.stop()
