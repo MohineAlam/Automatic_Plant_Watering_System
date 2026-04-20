a2adce882bd5a9c5d53373fc277685e0a47b7afa
MohineAlam <mohine.tasrin.alam@gmail.com>
2026-04-20 21:56:10 +0100

    ml prediction feature added

--- watering_system_v2/ml_based_watering/dashboard/plot_dashboard.py
+++ watering_system_v2/ml_based_watering/dashboard/plot_dashboard.py
@@ -1,3 +1,4 @@
+# library
 from flask import Flask, render_template
 import json
 import sys
@@ -7,13 +8,14 @@ import subprocess
 from datetime import datetime
 from gui.get_outside_temp import get_outside_temp
 from gui.test_sensor import read_sensor
-
+from ml_prediction.ml_whentowater_custom import run_model
 # graphs function
 from dashboard.plotly_graphs import multi_line_graph, multi_boxplot
 
 
 # plant diary input:
 input_data = json.load(open(sys.argv[1]))
+h_threshold = sys.argv[2]
 
 #============#
 # create app
@@ -33,16 +35,21 @@ def dashboard():
 	# multi boxplot
 	m_boxplot = multi_boxplot(input_data)
 	m_boxplot_html = m_boxplot.to_html(full_html = False)
+
 	# current data
 	day = str(datetime.now())
 	moisture = read_sensor()
 	temperature = get_outside_temp()
 
+	# ml prediction
+	ml_output = run_model(input_data,h_threshold)
+
 	return render_template("dashboard_template.html",
 		multi_line_graph=m_line_html,
 		multi_boxplot=m_boxplot_html,
 		moisture=moisture,
-		temperature=temperature)
+		temperature=temperature,
+		ml_answer=ml_output["message"])
 #=========#
 # run app
 #=========#
