c588e2856d603ad18c4fdc9cc6a9ececee9f2abd
MohineAlam <mohine.tasrin.alam@gmail.com>
2026-04-20 21:57:36 +0100

    make function into module and executable via cli

--- watering_system_v2/ml_based_watering/ml_prediction/ml_whentowater_custom.py
+++ watering_system_v2/ml_based_watering/ml_prediction/ml_whentowater_custom.py
@@ -1,9 +1,8 @@
 # library
 import json
-from decision_tree_ml import build_tree
-from decision_tree_ml import predict
-from test_sensor import read_sensor
-from test_pump import run_pump
+from ml_prediction.decision_tree_ml import build_tree, predict
+from gui.test_sensor import read_sensor
+from gui.test_pump import run_pump
 from datetime import datetime, timedelta
 import explorerhat
 import sys
@@ -14,80 +13,97 @@ import os
 if len(sys.argv) != 3:
         print("Correct usage: python ml_whentowater_custom.py diary.json volt_threshold")
         sys.exit(2)
-
-# training data-set
-x = [] # import data from plant diary
-y = [] # has plant been watered in the last 24hrs? 1 = yes , 0 = no ** model will give prediction in 1 or 0
-
 # load plant data - must be one plant at a time
 plantdiary = json.load(open(sys.argv[1]))
 volt_threshold = sys.argv[2]
 
-
 # time parser
 def time_parser(day):
-        time = datetime.fromisoformat(day)
-        return time
+	time = datetime.fromisoformat(day)
+	return time
 
-# watered days
-watered_days = []
+def run_model(plantdiary, volt_threshold):
+	# training data-set
+	x = [] # import data from plant diary
+	y = [] # has plant been watered in the last 24hrs? 1 = yes , 0 = no ** model will give prediction in 1 or 0
 
-# loop through metrics of the plant (day, voltage, water_day, water_voltage)
-for plant, metrics in plantdiary.items():
-        # plant watered and voltage taken
-        for index in range(len(metrics["water_day"])):
-                wt = time_parser(metrics["water_day"][index])
-                watered_days.append(wt)
-        # plant voltage and day taken
-        for index in range(len(metrics["day"])):
-                 # parse_date creates = (2026, 01, 23, 21, 55, 09) date format
-                time = datetime.fromisoformat(metrics["day"][index])
-                # humidity of each day
-                humidity = metrics["voltage"][index]
-                # water_day that was recored before current time
-                past_watering = [wt for wt in watered_days if wt < time]
-                # how many days ago was the plant last watered comapred to current time
-                days_since_last_watered = (time - max(past_watering)).days if past_watering else 1000
-                # returns true or false if any water_day in list is within the next 24 hrs of the current time
-                watered_soon = any(time < wt < time + timedelta(hours=24) for wt in watered_days)
 
-                # append x and y values
-                x.append([humidity, days_since_last_watered, time.hour])
-                y.append(1 if watered_soon is True or humidity < 0.1 else 0)
+	# watered days
+	watered_days = []
 
-# build a tree with plant diary
-x = np.array(x)
-y = np.array(y)
-tree = build_tree(x,y)
+	# loop through metrics of the plant (day, voltage, water_day, water_voltage)
+	for plant, metrics in plantdiary.items():
+		# plant watered and voltage taken
+		for index in range(len(metrics["water_day"])):
+			wt = time_parser(metrics["water_day"][index])
+			watered_days.append(wt)
+		# plant voltage and day taken
+		for index in range(len(metrics["day"])):
+			 # parse_date creates = (2026, 01, 23, 21, 55, 09) date format
+			time = datetime.fromisoformat(metrics["day"][index])
+			# humidity of each day
+			humidity = metrics["voltage"][index]
+			# water_day that was recored before current time
+			past_watering = [wt for wt in watered_days if wt < time]
+			# how many days ago was the plant last watered comapred to current time
+			days_since_last_watered = ((time - max(past_watering)).days if past_watering else 1000)
+			# returns true or false if any water_day in list is within the next 24 hrs of the current time
+			watered_soon = any(time < wt < time + timedelta(hours=24) for wt in watered_days)
+			# append x and y values
+			x.append([humidity, days_since_last_watered, time.hour])
+			y.append(1 if watered_soon is True or humidity < 0.1 else 0)
 
-# take measurement of plant and populate list
-x2 = []
-# voltage
-#humidity = read_sensor()
-humidity = 0.01
-# date stamp
-day = datetime.now().isoformat()
-time2 = time_parser(day)
-# days since last watered
-days_since_laste_watered2 = ((time2 - max(watered_days)).days if watered_days else 1000)
-# append to x2
-x2.append([humidity, days_since_laste_watered2, time2.hour])
-x2 = np.array(x2)
-# create prediction
-prediction = predict(x2, tree)
+	# build a tree with plant diary
+	x = np.array(x)
+	y = np.array(y)
+	tree = build_tree(x,y)
 
-if prediction == 1:
-	# activate pump
-	print("Plant needs watering! :(")
-	os.system("mpv --speed=0.7  ~/Automatic_Plant_Watering_System/voice_clips/I_need_water.m4a")
-	run_pump()
-	print("Plant successfully watered! :)")
-elif prediction == 0:
-	# message to keep pump off
-	print("No watering needed! :)")
-	print("mpv --speed=1.5  ~/Automatic_Plant_Watering_System/voice_clips/Im_good.m4a")
-	print(f"Humidity value: {humidity}")
-else:
-	# error message
-	print("There was an error! :(")
-	sys.exit(1)
+	# take measurement of plant and populate list
+	x2 = []
+	# voltage
+	humidity = read_sensor()
+	humidity = print(f"Humidity value: {humidity}")
+	# date stamp
+	day = datetime.now().isoformat()
+	time2 = time_parser(day)
+	# days since last watered
+	days_since_last_watered2 = ((time2 - max(watered_days)).days if watered_days else 1000)
+	# append to x2
+	x2.append([humidity, days_since_last_watered2, time2.hour])
+	x2 = np.array(x2)
+	# create prediction
+	prediction = predict(x2, tree)
+
+	if prediction == 1:
+		# activate pump
+		message = "Plant needs watering! :("
+		os.system("mpv --speed=0.7  ~/Automatic_Plant_Watering_System/voice_clips/I_need_water.m4a")
+
+	elif prediction == 0:
+		# message to keep pump off
+		message = "No watering needed! :)"
+		print("mpv --speed=1.5  ~/Automatic_Plant_Watering_System/voice_clips/Im_good.m4a")
+
+	else:
+		# error message
+		message = print("There was an error! :(")
+
+	return {
+
+		"prediction" : prediction,
+		"humidity" : humidity,
+		"message" : message
+	}
+
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print("Correct usage: python ml_whentowater_custom.py diary.json volt_threshold")
+		sys.exit(2)
+	# load plant data - must be one plant at a time
+	plantdiary = json.load(open(sys.argv[1]))
+	volt_threshold = sys.argv[2]
+	output = run_model(plantdiary, volt_threshold)
+	print(output["message"])
+	print(output["humidity"])
+	if output["prediction"] == 1:
+		run_pump()
